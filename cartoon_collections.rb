ae4fa2261a6dfbc11b43416677c8d14e6a9b0284
PepperMcLean <jackmcl206@gmail.com>
2021-02-12 15:39:31 -0600

    commit tree eb9dbdc3c02d0463d9da92736c131fb8afad7133

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map! { |name| name.capitalize }
+  array.map! { |name| name + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |name|
+    if name.length > 4
+      return true
+    end 
+  end
+    false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |stuff|
+    cheese_types.each do |cheese|
+      if stuff.include? cheese
+        return cheese
+      end
+    end
+  end
+  nil
 end
